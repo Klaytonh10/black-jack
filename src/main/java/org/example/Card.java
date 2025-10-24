1b4f283c80e866f6084176d26be9722b117df520
Klaytoh10 <klaytonh10@gmail.com>
2025-10-24 14:17:28 -0500

    Fix bigs in classes and testing methods

--- src/main/java/org/example/Card.java
+++ src/main/java/org/example/Card.java
@@ -1,43 +1,76 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Card {
+
     private String suit;
-    private String value;
+    private String rank;
     private boolean isFaceUp;
-    public Card(String suit, String value) {
+
+    public Card(String suit, String rank) {
         this.suit = suit;
-        this.value = value;
+        this.rank = rank;
         this.isFaceUp = false;
     }
     public String getSuit(){
-// only return the suit if the card is face up
+        // only return the suit if the card is face up
         if(isFaceUp){
             return suit;
         } else {
             return "#";
         }
     }
-    public String getValue(){
-// only return the value if the card is face up
+    public String getRank(){
+    // only return the value if the card is face up
         if(isFaceUp){
-// this is the string value of the card
-// i.e. A, K, Q, J, 10, 9 ...
-            return value;
+        // this is the string value of the card
+        // i.e. A, K, Q, J, 10, 9 ...
+            return rank;
         } else {
             return "#";
         }
     }
     public int getPointValue(){
-// only return the value if the card is face up
-        if(isFaceUp){
-// determine point value and return it
-// A = 11
-// K, Q, J = 10
-// all numeric cards are equal to their face value
-            return 0;
+        // only return the value if the card is face up
+        int pointValue;
+        Scanner scanner = new Scanner(System.in);
+        if(this.isFaceUp){
+            switch (this.rank) {
+                case "2":
+                    return 2;
+                case "3":
+                    return 3;
+                case "4":
+                    return 4;
+                case "5":
+                    return 5;
+                case "6":
+                    return 6;
+                case "7":
+                    return 7;
+                case "8":
+                    return 8;
+                case "9":
+                    return 9;
+                case "10":
+                    return 10;
+                case "J", "Q", "K":
+                    return 10;
+                case "A":
+                    System.out.print("Do you want 1 or 11: ");
+                    int value = scanner.nextInt();
+                    return value;
+            }
+            // TODO: fix this
+        // determine point value and return it
+        // A = 11
+        // K, Q, J = 10
+        // all numeric cards are equal to their face value
         } else {
             return 0;
         }
+        return 0;
     }
     public boolean isFaceUp(){
         return isFaceUp;
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        Deck deck = new Deck();
 
+        deck.shuffle();
+
+        Hand john = new Hand();
+        Hand greg = new Hand();
+
+        john.getValue();
     }
 }
